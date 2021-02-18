404cf8ab20040cc2c8eea0ec20823ae1689c39b4
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-18 22:41:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -17,7 +17,7 @@ def prime?(integer)
   return true if integer == 2
   return false if integer <= 0
   return false if integer % 2 == 0
-  return false if integer == 1
+  #return false if integer == 1
   i = 2
   while i < integer.floor
     return false if integer % i  == 0
